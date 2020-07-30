82c755ddeb5b1bb980d392eda110f9a1a77b1d25
Fabio Menozzi <f.menozzi@yahoo.com>
2020-07-30 19:10:17 -0300

    table created

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 let AWS = require('aws-sdk');
 
 AWS.config.update({
-    region: 'us-west-2',
+    region: 'sa-east-1',
     endpoint: 'http://localhost:8000'
 });
 
-let dynamodb = AWS.DynamoDB();
+let dynamodb = new AWS.DynamoDB();
 let params = {
     
     TableName: 'NodeJsBaseballStats',
@@ -24,3 +24,11 @@ let params = {
         WriteCapacityUnits: 5
     }
 };
+
+dynamodb.createTable(params, (err, data) => {
+    if (err) {
+        console.error('Unable to create table!', JSON.stringify(err));
+    } else {
+        console.log('Table created!');
+    }
+})
