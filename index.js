71c67c99e8d640ec2a2824d75597c56cb1058d1c
Fabio Menozzi <f.menozzi@yahoo.com>
2020-07-30 18:55:56 -0300

    config partitioKey sortKey and RW Capacity

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 //Create table
 
-var AWS = require('aws-sdk');
+let AWS = require('aws-sdk');
 
 AWS.config.update({
     region: 'us-west-2',
     endpoint: 'http://localhost:8000'
 });
+
+let dynamodb = AWS.DynamoDB();
+let params = {
+    
+    TableName: 'NodeJsBaseballStats',
+    KeySchema: [
+        { AttributeName: 'TeamID', KeyType: 'HASH' }, //Partitionkey
+        { AttributeName: 'SK', KeyType: 'RANGE' }, //SortKey
+    ],
+    AttributeDefinitions: [
+        { AttributeName: 'TeamID', AttributeType: 'S' },
+        { AttributeName: 'SK', AttributeType: 'S' },
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
+        WriteCapacityUnits: 5
+    }
+};
